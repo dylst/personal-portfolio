7de84edce3368f1b5baf6cb2fc543c061390e304
dylst <cdylan127@gmail.com>
2023-04-06 15:28:45 -0700

    Fix minor styling issue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -121,7 +121,7 @@ const NavBar = () => {
 
           <button
             onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
-            className={`ml-3 flex items-center justify-center rounded-full p-1 ease
+            className={`ml-3 w-8 flex items-center justify-center rounded-full p-1 ease
       ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}
       `}
           >
